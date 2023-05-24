4fb048b4614c27414d9c5b918481d23a82da86cd
Katsiaryna Kulik <125984123+KatsiarynaKulik@users.noreply.github.com>
2023-05-24 23:07:40 +0400

    Fixed: grammatical errors

--- AwasomeBrowser/AppDelegate.swift
+++ AwasomeBrowser/AppDelegate.swift
@@ -2,7 +2,8 @@
 //  AppDelegate.swift
 //  AwasomeBrowser
 //
-//  Created by Екатерина  on 24.03.23.
+//  Created by Katsiaryna Kulik  on 24.03.23.
+//  Copyright © 2023 Katsiaryna Kulik. All rights reserved.
 //
 
 import UIKit
